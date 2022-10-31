f644bf14ba93e5f554049ab9bcdf47c455653a26
flembee <flembee.technologies@gmail.com>
2022-10-31 12:57:37 -0400

    Adding tables bin creation and methods in roles  services

--- sql/src/app/services/roles.service.js
+++ sql/src/app/services/roles.service.js
@@ -4,38 +4,67 @@ class RolesService {
     }
 
     async search() {
-        this.db.query("SELECT * FROM roles", function (err, result, fields) {
-            if (err) throw err;
-            console.log(result);
-        });
+        const roles = await this.db.Role.findAll();
+
+        if (!roles)
+            return "No data"
+
+        return roles;
     }
 
     async get(id) {
-        this.db.query("SELECT * FROM roles", function (err, result, fields) {
-            if (err) throw err;
-            console.log(result);
+        const role = await this.db.Role.findOne({
+            where: {id},
         });
+
+        if (!role)
+            return "No data"
+
+        return role;
     }
 
     async add(data) {
-        this.db.query("SELECT * FROM roles", function (err, result, fields) {
-            if (err) throw err;
-            console.log(result);
-        });
+        const { name } = data
+
+        const role = await this.db.Role.create({name});
+
+        if (!role)
+            return "No data"
+
+        return role;
     }
 
-    async delete(nodeData) {
-        this.db.query("SELECT * FROM roles", function (err, result, fields) {
-            if (err) throw err;
-            console.log(result);
+    async delete(id) {
+        const role = await this.db.Role.findOne({
+            where: {id}
         });
+
+        if (!role)
+            return "No data"
+
+        await role.destroy();
+
+        return role;
     }
 
     async update(id, data) {
-        this.db.query("SELECT * FROM roles", function (err, result, fields) {
-            if (err) throw err;
-            console.log(result);
+        const { name } = data
+
+        const role = await this.db.Role.findOne({
+            where: {id}
         });
+
+        if (!role)
+            return "No data"
+
+        const roleUpdated = {
+            ...role, 
+            name: name,
+        };
+
+        await roleUpdated.save();
+
+        return roleUpdated;
     }
 }
 
